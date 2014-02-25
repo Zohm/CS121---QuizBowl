cf76825344ffd2dbc9c2e5f94530b46bda08d1d0
Zohm <charles-vs-bg@hotmail.fr>
2014-02-25 14:03:00 -0800

    New version of the app
    Here is the new project I started.

---
+++ Quiz-Bowl_NewVersion/Quiz-Bowl/CHHTPlayViewController.m
@@ -0,0 +1,38 @@
+//
+//  CHHTPlayViewController.m
+//  Quiz-Bowl
+//
+//  Created by Zohm on 25/02/14.
+//  Copyright (c) 2014 Zohm. All rights reserved.
+//
+
+#import "CHHTPlayViewController.h"
+
+@interface CHHTPlayViewController ()
+
+@end
+
+@implementation CHHTPlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Quiz-Bowl_NewVersion/Quiz-Bowl/CHHTViewController.m
@@ -0,0 +1,38 @@
+//
+//  CHHTViewController.m
+//  Quiz-Bowl
+//
+//  Created by Zohm on 25/02/14.
+//  Copyright (c) 2014 Zohm. All rights reserved.
+//
+
+#import "CHHTViewController.h"
+
+@interface CHHTViewController ()
+
+@end
+
+@implementation CHHTViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
