0cf36652b1ecfaa7a200f3a18f112404667ed887
Zohm <charles-vs-bg@hotmail.fr>
2014-03-04 02:11:00 -0800

    Timer implemented

---
+++ Quiz-Bowl  04:03:2014/Quiz-Bowl/CHHTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHHTAppDelegate.h
+//  Quiz-Bowl
+//
+//  Created by Zohm on 24/02/14.
+//  Copyright (c) 2014 Zohm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHHTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz-Bowl  04:03:2014/Quiz-Bowl/config.h
@@ -0,0 +1,20 @@
+//
+//  config.h
+//  Quiz-Bowl
+//
+//  Created by Zohm on 03/03/14.
+//  Copyright (c) 2014 Zohm. All rights reserved.
+//
+
+#ifndef Quiz_Bowl_config_h
+#define Quiz_Bowl_config_h
+
+// Screen dimensions
+#define screenWidth [UIScreen mainScreen].bounds.size.height
+#define screenHeight [UIScreen mainScreen].bounds.size.width
+
+// Font
+#define fontHUD [UIFont fontWithName:@"comic andy" size:45.0]
+#define fontHUDBig [UIFont fontWithName:@"comic andy" size:100.0]
+
+#endif
